a194f112fd46513f6d5a5d617ba68958a54fe22d
CristyVal <134471647+CristyVal@users.noreply.github.com>
2023-08-15 22:37:42 -0600

    Delete src directory
    Eliminar carpeta

--- src/services/firebase/auth.js
+++
@@ -1,9 +0,0 @@
-const {getAuth} = require("firebase-admin/auth");
-const conexion = require("./");
-
- function firebaseAuth(req, res, next) {
-    req.auth = getAuth(conexion);
-    next()
-}
-
-module.exports = { firebaseAuth };
